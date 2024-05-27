8cb7618951604b99f881329c04887521797096c3
itc-s23006 <s23006@std.it-college.ac.jp>
2024-05-27 09:12:48 +0900

    Create d48Q27kadai.java

---
+++ d48Q27kadai.java
@@ -0,0 +1,12 @@
+// s23006
+// d48Q27kadai の実行結果を無限ループを修正
+
+public class d48Q27kadai {
+		public static void main(String[] args) {
+				int i = 0;
+				while (i <= 6) {
+						System.out.println(i);
+						i++;
+				}
+		}
+}

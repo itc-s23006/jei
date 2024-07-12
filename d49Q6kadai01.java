d7f062186abe1c0b669c4a49d01a21f6caf217f7
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-12 02:57:50 +0000

    Change File Name d49Q6kadai.java -> d49Q6kadai01.java

---
+++ d49Q6kadai01.java
@@ -0,0 +1,16 @@
+// s23006
+// 与えられた引数を連結して表示するように、StringBuilderクラスを使ってプログラムを改造。
+
+public class d49Q6kadai01 {
+    public static void main(String[] arguments) {
+        // StringBuilder を使って引数を連結
+        StringBuilder sb = new StringBuilder();
+        for (String arg : arguments) {
+            sb.append(arg);
+        }
+        
+        // 結果を表示
+        System.out.println(sb.toString());
+    }
+}
+

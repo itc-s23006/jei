659f0a0bae8abef1b0d7164d7ac4cfeea39a234b
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-12 02:16:57 +0000

    Modification d49Q5kadai.java

--- d49Q5kadai.java
+++ d49Q5kadai.java
@@ -1,27 +1,50 @@
-public class Q5 {
-		public static void main(String[] args) {
-				int limit = Integer.parseInt(args[0]);
-				int base1 = Interger.parseInt(args[1]);
-				int base2 = Interger.parseInt(args[2]);
+public class d49Q5kadai {
+    public static void main(String[] args) {
+        // 入力引数から値を取得
+        int limit = Integer.parseInt(args[0]);
+        int base1 = Integer.parseInt(args[1]);
+        int base2 = Integer.parseInt(args[2]);
+        int base3 = Integer.parseInt(args[3]);
 
-				int common = base1 * base2;
+        // 共通の倍数を計算
+        int common12 = base1 * base2;
+        int common13 = base1 * base3;
+        int common23 = base2 * base3;
+        int common123 = base1 * base2 * base3;
 
-				for (int i = 1; i <= limit; i++) {
-						if (i % common == 0) {
-								System.out.println(i + ":base1,base2");
-								continue;
-						}
-						if (i % base1 == 0) {
-								System.out.println(i + ":base1");
-								continue;
-						}
-						if (i % base2 == 0) {
-								System.out.println(i + ":base2");
-								continue;
-						}
-						System.out.println(i);
-				}
-		}
+        // limit までの数をループ
+        for (int i = 1; i <= limit; i++) {
+            // 各基数に対する条件をチェック
+            if (i % common123 == 0) {
+                System.out.println(i + ":base1,base2,base3");
+                continue;
+            }
+            if (i % common12 == 0) {
+                System.out.println(i + ":base1,base2");
+                continue;
+            }
+            if (i % common13 == 0) {
+                System.out.println(i + ":base1,base3");
+                continue;
+            }
+            if (i % common23 == 0) {
+                System.out.println(i + ":base2,base3");
+                continue;
+            }
+            if (i % base1 == 0) {
+                System.out.println(i + ":base1");
+                continue;
+            }
+            if (i % base2 == 0) {
+                System.out.println(i + ":base2");
+                continue;
+            }
+            if (i % base3 == 0) {
+                System.out.println(i + ":base3");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
-
 

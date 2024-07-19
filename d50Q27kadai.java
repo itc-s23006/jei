885a339802fadba86891b4b9a0cf1f3644509288
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-19 02:09:39 +0000

    Modification d50Q27kadai.java

--- d50Q27kadai.java
+++ d50Q27kadai.java
@@ -1,17 +1,25 @@
-class d50Q27kadai {
-		public static void main(String[] args) {
-				int control = 3;
-				String mode;
+// s23006
+// コマンドラインから与えられた整数を指定できるように改造。
 
-				switch (control) {
-						case 1:
-								mode = "Warm";
-						case 2:
-								mode = "Cool";
-						default:
-								mode = "Wind";
-								break;
-				}
-				System.out.println(mode);
-		}
+class d50Q27kadai {
+    public static void main(String[] args) {
+        // コマンドライン引数を整数に変換
+        int control = Integer.parseInt(args[0]);
+        String mode;
+
+        switch (control) {
+            case 1:
+                mode = "Warm";
+                break;
+            case 2:
+                mode = "Cool";
+                break;
+            case 3:
+            default:
+                mode = "Wind";
+                break;
+        }
+        System.out.println(mode);
+    }
 }
+

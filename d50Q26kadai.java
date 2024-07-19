7e83b522727a2735541d6bae2365ccb3957f88da
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-19 01:44:46 +0000

    Create d50Q26kadai.java

---
+++ d50Q26kadai.java
@@ -0,0 +1,8 @@
+enum Condition {ROAD, MUD, SNOW}
+
+class d50Q26kadai {
+		public static void main(String[] args) {
+				Condition c = Condition.SNOW;
+				System.out.println("Condition is " + c);
+		}
+}

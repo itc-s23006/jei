ea24312ca07ca171933ffcb627e235b0b0bd5e3e
itc-s23006 <s23006@std.it-college.ac.jp>
2024-08-02 02:09:20 +0000

    改造

--- d51Q5kadai.java
+++ d51Q5kadai.java
@@ -1,23 +1,24 @@
 public class d51Q5kadai {
-		public static void main(String[] args) {
-				double[] level = {0.0, 24.0, 57.0, Double.MAX_VALUE};
-				double[] unit = {0.0, 2.0, 3.0, 5.0};
-				int[] rate = {0, 19, 17, 14};
-				if (args.length != 1) {
-						System.out.println("引数の個数が不足");
-						return;
-				}
-				double amount = Double.parseDouble(args[0]);
-				if (amount < 0.0) {
-						System.out.println("0.0以上の使用量を入力");
-						return;
-				}
+    public static void main(String[] args) {
+        double[] level = {0.0, 24.0, 57.0, Double.MAX_VALUE};
+        double[] unit = {0.0, 2.0, 3.0, 5.0};
+        int[] rate = {1, 20, 18, 15}; // 各1円値上げした従量単価
+        if (args.length != 1) {
+            System.out.println("引数の個数が不足");
+            return;
+        }
+        double amount = Double.parseDouble(args[0]);
+        if (amount < 0.0) {
+            System.out.println("0.0以上の使用量を入力");
+            return;
+        }
 
-				int i, price = 1200;
-				for (i = 1; amount >= level[i]; i++) {
-						price += (int)((level[i] - level[i - 1]) / unit[i]) * rate[i];
-				}
-				price += (int) Math.ceil((amount - level[i - 1]) / unit[i]) * rate[i];
-				System.out.println("今月のガス料金 " + price + "円");
-		}
+        int i, price = 1300; // 基本使用料を100円値上げ
+        for (i = 1; amount >= level[i]; i++) {
+            price += (int)((level[i] - level[i - 1]) / unit[i]) * rate[i];
+        }
+        price += (int) Math.ceil((amount - level[i - 1]) / unit[i]) * rate[i];
+        System.out.println("今月のガス料金 " + price + "円");
+    }
 }
+

5ad19fdbd83b707325b11f27e8248d7b4c0fbbf4
itc-s23006 <s23006@std.it-college.ac.jp>
2024-08-09 01:10:15 +0000

    改造

--- d51Q26kadai.java
+++ d51Q26kadai.java
@@ -1,15 +1,15 @@
+// s23006
+// 「-> 」演算子を使ったプログラム。
+
 public class d51Q26kadai {
-		public static void main(String[] args) {
-				int n = 1;
-				String s = "";
-				switch (n) {
-						case 1:
-								s += "one";
-						case 2:
-								s += "two";
-						default:
-								s += "?";
-				}
-				System.out.println(s);
-		}
+    public static void main(String[] args) {
+        int n = 1;
+        String s = switch (n) {
+            case 1 -> "one";
+            case 2 -> "two";
+            default -> "?";
+        };
+        System.out.println(s);
+    }
 }
+

be42d551b6ca8119cc54183a1fc8d3092f2db916
itc-s23006 <s23006@std.it-college.ac.jp>
2024-08-09 02:10:46 +0000

    改造

--- d52Q27kadai.java
+++ d52Q27kadai.java
@@ -1,15 +1,20 @@
-enum Singnal {
-		RED, YELLOW, GREEN;
+// s23006
+// 信号機の状態を判定して表示するプログラム
+
+enum Signal {
+    RED, YELLOW, GREEN;
 }
+
 public class d52Q27kadai {
-		public static void main(String[] args) {
-				Signal sig = YELLOW;
-				if (sig == Signal.GREEN) {
-						System.out.println("緑");
-				} else if (sig == Signal.YELLOW) {
-						System.out.println("黃");
-				} else {
-						System.out.println("赤");
-				}
-		}
+    public static void main(String[] args) {
+        Signal sig = Signal.YELLOW; // Signal 型のインスタンスを正しく初期化
+        if (sig == Signal.GREEN) {
+            System.out.println("緑");
+        } else if (sig == Signal.YELLOW) {
+            System.out.println("黄");
+        } else {
+            System.out.println("赤");
+        }
+    }
 }
+
